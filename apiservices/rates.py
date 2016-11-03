e62f16b0f309b2ef82008634362d0aca9c48c211
John Crawford <jrcrawfo@ncsu.edu>
2016-11-03 13:14:09 -0400

    Added more rates endpoints

--- apiservices/rates.py
+++ apiservices/rates.py
@@ -2,19 +2,27 @@ from apiservices import *
 
 class RatesService(Service):
 
-    clientID = 'IhnocBdLJ0UBmAJ3w7HW6CbwlpPKHj2Y'
+    def __init__(self, client_id, client_secret=None):
 
-    def __init__(self, client):
-
-        super(RatesService, self).__init__(client)
+        super(RatesService, self).__init__(client_id, client_secret)
 
     def baseURL(self):
 
         return 'https://rates.api.ndustrial.io'
 
+    def audience(self):
+
+        return 'IhnocBdLJ0UBmAJ3w7HW6CbwlpPKHj2Y'
+
     def getSchedules(self, execute=True):
 
         return self.execute(GET(uri='schedules'), execute)
 
-
+    def getSchedulePeriods(self, id, execute=True):
+        
+        return self.execute(GET(uri='schedules/{}/periods'.format(id)), execute)
+    
+    def getPeriod(self, id, execute=True):
+        
+        return self.execute(GET(uri='periods/{}'.format(id)), execute)
 
