832f6c8253936bcfd6bc63c2b9b18ee9986bebb1
John Crawford <jrcrawfo@ncsu.edu>
2016-11-16 08:53:04 -0500

    Addition of some flywheeling endpoints

---
+++ apiservices/flywheeling.py
@@ -0,0 +1,33 @@
+from apiservices import *
+
+class FlywheelingService(Service):
+
+    def __init__(self, client_id, client_secret=None):
+
+        super(FlywheelingService, self).__init__(client_id, client_secret)
+
+    def baseURL(self):
+
+        return 'https://flywheeling.api.ndustrial.io'
+
+    def audience(self):
+
+        return 'GvjVT0O7PO1biyzABeqInlodVbN9TsCf'
+
+    def getFacilities(self, execute=True):
+
+        return self.execute(GET(uri='facilities'), execute)
+    
+    
+    def getSystemsForFacility(self, facility_id, execute=True):
+        return self.execute(GET(uri='facilities/{}/systems'.format(facility_id)), execute)
+        
+    def getZonesForSystem(self, system_id, execute=True):
+        return self.execute(GET(uri='systems/{}/zones'.format(system_id)), execute)
+    
+    def createRun(self, run_obj):
+        return self.execute(POST(uri='runs').body(run_obj))
+        
+    def addDataToRun(self, run_id, run_data):
+        return self.execute(POST(uri='runs/{}/data'.format(run_id)).body(run_data))
+
