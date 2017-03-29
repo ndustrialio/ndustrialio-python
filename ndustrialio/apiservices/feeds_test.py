9a2a38b8c6205c842f7cac543229bca8a8585e1b
AaronNewman <agn9@duke.edu>
2017-03-29 16:35:33 -0400

    Removed print statement

--- ndustrialio/apiservices/feeds_test.py
+++ ndustrialio/apiservices/feeds_test.py
@@ -3,4 +3,3 @@ from feeds import FeedsService
 if __name__ == '__main__':
     service = FeedsService(client_id='hcICuGIbxMd24J32Bdiw9moTaUb7ZQcG', client_secret='ctc8TUaYuGBa7Tc7mazEtRI1IHTsbM58Q9kFuWxV00pGNHZOoyl2IQ4tpnIpSBHB')
     data = service.getHourlyMetrics('egauge19395', 'compressor_c1-p.demand')
-    print('here')
