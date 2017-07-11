9f9d6adbe52aa09ccb498553e94396ed892f333f
AaronNewman <agn9@duke.edu>
2017-07-11 18:14:00 -0400

    Changed ordering of table creation

--- ndustrialio/apiservices/tests/integration_tests/fixtures/setup_get_feeds.sql
+++ ndustrialio/apiservices/tests/integration_tests/fixtures/setup_get_feeds.sql
@@ -1,3 +1,7 @@
+CREATE TABLE IF NOT EXISTS feed_types(feed_type varchar(50), down_after integer);
+DELETE FROM feed_types;
+INSERT INTO feed_types(feed_type, down_after) VALUES
+('test_type', 60);
 CREATE TABLE IF NOT EXISTS feeds(feed_type_id integer, key varchar(20), facility_id integer, timezone varchar(20), routing_keys text, token varchar(100), degraded_threshold float, critical_threshold float, created_at timestamp, updated_at timestamp);
 DELETE FROM feeds;
 INSERT INTO feeds(feed_type_id, key, facility_id, timezone, routing_keys, token, degraded_threshold, critical_threshold, created_at, updated_at) VALUES
@@ -8,7 +12,3 @@ INSERT INTO feeds(feed_type_id, key, facility_id, timezone, routing_keys, token,
 (1, 'key_5', 50, 'UTC', '[routing_key_5]', 'token_5', 0.8, 0.2, '1999-05-08 04:05:06', '2000-05-08 04:05:06'),
 (1, 'key_6', 60, 'UTC', '[routing_key_6]', 'token_6', 0.8, 0.2, '1999-06-08 04:05:06', '2000-06-08 04:05:06'),
 (1, 'key_7', 70, 'UTC', '[routing_key_7]', 'token_7', 0.8, 0.2, '1999-07-08 04:05:06', '2000-07-08 04:05:06');
-CREATE TABLE IF NOT EXISTS feed_types(feed_type varchar(50), down_after integer);
-DELETE FROM feed_types;
-INSERT INTO feed_types(feed_type, down_after) VALUES
-('test_type', 60);
