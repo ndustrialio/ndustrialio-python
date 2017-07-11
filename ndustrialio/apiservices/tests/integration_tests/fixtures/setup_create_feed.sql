465460235c79bec792f2e8c4352bca22d715c528
AaronNewman <agn9@duke.edu>
2017-07-11 16:37:56 -0400

    Added mock for feeds service base url

--- ndustrialio/apiservices/tests/integration_tests/fixtures/setup_create_feed.sql
+++ ndustrialio/apiservices/tests/integration_tests/fixtures/setup_create_feed.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS feeds(feed_type_id integer, key varchar(20), facility_id integer, timezone varchar(20), routing_keys text, token varchar(100), degraded_threshold float, critical_threshold float, created_at timestamp default current_timestamp);
+CREATE TABLE IF NOT EXISTS feeds(feed_type_id integer, key varchar(20), facility_id integer, timezone varchar(20), routing_keys text, token varchar(100), degraded_threshold float, critical_threshold float, created_at timestamp, updated_at timestamp);
 DELETE FROM feeds;
