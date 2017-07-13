2ad6630c8ce50fb7819620151f0a29079e8664d9
AaronNewman <agn9@duke.edu>
2017-07-13 13:03:40 -0400

    realtime service working

--- test_runner.py
+++ test_runner.py
@@ -4,7 +4,5 @@ from ndustrialio.workertools.test_runner import TestRunner
 import os
 
 if __name__ == '__main__':
-    print 'postgres host: {}'.format(os.environ.get('POSTGRES_HOST'))
-    print 'redis host: {}'.format(os.environ.get('REDIS_HOST'))
     test_runner = TestRunner()
     test_runner.runTestCases([TestAPIClient, TestFeeds])
