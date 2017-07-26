5502a8ea0d184050dadc2f8b85feb36e72f2e27e
AaronNewman <agn9@duke.edu>
2017-07-26 17:23:58 -0400

    Added cassandra and postgres test utilities

--- ndustrialio/workertools/test_runner.py
+++ ndustrialio/workertools/test_runner.py
@@ -6,17 +6,19 @@ class TestRunner:
     def __init__(self):
         self.loader = unittest.TestLoader()
 
-    def runTests(self, test_cases=None, raise_error=True):
+    def runTests(self, test_cases=[], test_modules=[], raise_error=True):
 
         suite = unittest.TestSuite()
 
         for test_case in test_cases:
             suite.addTests(self.loader.loadTestsFromTestCase(test_case))
 
-        test_result = unittest.TextTestRunner(verbosity=2).run(suite)
-        failed_tests = test_result.failures
+        for test_module in test_modules:
+            suite.addTests(self.loader.loadTestsFromModule(test_module))
 
-        if failed_tests and raise_error:
-            raise Exception('Test failures: {}'.format(failed_tests))
+        test_result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+        if not test_result.wasSuccessful() and raise_error:
+            raise Exception('Some tests did not pass')
         else:
             return test_result
