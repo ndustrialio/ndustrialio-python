bcf741c47b1fc4588ed39ffd947f27f4156e6b9e
jrcrawfo <jrcrawfo@gmail.com>
2018-01-17 08:51:05 -0500

    Added some endpoints for creating flywheeling areas

--- ndustrialio/apiservices/flywheeling.py
+++ ndustrialio/apiservices/flywheeling.py
@@ -16,48 +16,48 @@ class FlywheelingService(Service):
 
     def getFacilities(self, execute=True):
         return self.execute(GET(uri='facilities'), execute)
-    
+
     def getSystem(self, system_id, execute=True):
         return self.execute(GET(uri='systems/{}'.format(system_id)), execute)
-    
+
     def getSystemsForFacility(self, facility_id, execute=True):
         return self.execute(GET(uri='facilities/{}/systems'.format(facility_id)), execute)
-        
+
     def getZonesForSystem(self, system_id, execute=True):
         return self.execute(GET(uri='systems/{}/zones'.format(system_id)), execute)
-    
+
     def createRun(self, run_obj):
         return self.execute(POST(uri='runs').body(run_obj))
-        
+
     def addDataToRun(self, run_id, run_data):
         return self.execute(POST(uri='runs/{}/data'.format(run_id)).body(run_data))
-    
+
     def getFacilityAreas(self, facility_id, execute=True):
         return self.execute(GET(uri='facilities/{}/areas'.format(facility_id)), execute)
-    
+
     def addSensorToArea(self, area_id, sensor_id, execute=True):
         return self.execute(POST(uri='areas/{}/sensors/{}'.format(area_id, sensor_id)))
 
     def getSensorsForArea(self, area_id,limit=10, offset=0, execute=True):
         params = {'limit': limit,
                   'offset': offset}
-                  
+
         return PagedResponse(self.execute(GET(uri='areas/{}/sensors'.format(area_id)).params(params), execute))
-    
+
     ## Scheme-based run
     def createRunForZone(self, zone_id, run_obj):
-        
+
         assert isinstance(zone_id, str)
         assert isinstance(run_obj, dict)
         return self.execute(POST(uri='/zones/{}/runs'.format(zone_id)).body(run_obj))
-    
+
     ## Scheme-based run data
     def addDataToZoneRun(self, run_id, run_data):
-        
+
         assert isinstance(run_id, int)
         assert isinstance(run_data, dict)
         return self.execute(POST(uri='schemes/runs/{}/data'.format(run_id)).body(run_data))
-    
+
     def getSystemSetpointData(self, system_id, execute=True):
         assert isinstance(system_id, str)
         return self.execute(GET(uri='systems/{}/schemes/data'.format(system_id)), execute)
@@ -156,3 +156,23 @@ class FlywheelingService(Service):
         assert isinstance(facility_id, int)
         assert isinstance(run_id, int)
         return self.execute(DELETE(uri='facilities/{}/runs/{}/output'.format(facility_id, run_id)), execute)
+
+    # Creation of a zone
+    def createZoneForSystem(self, system_id, name, label, execute=True):
+        assert isinstance(system_id, str)
+        assert isinstance(name, str)
+        assert isinstance(label, str)
+
+        body = {'system_id': system_id,
+                'name': name,
+                'label': label}
+        return self.execute(POST(uri='zones').body(body), execute)
+
+    # Creation of an area
+    def createAreaForFacility(self, facility_id, label, execute=True):
+        assert isinstance(facility_id, int)
+        assert isinstance(label, str)
+
+        body = {'facility_id': facility_id,
+                'label': label}
+        return self.execute(POST(uri='areas').body(body), execute)
