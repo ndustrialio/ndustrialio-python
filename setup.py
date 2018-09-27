a094e94ae73c73c2bbb83c8990aa94837dcb43e6
jrcrawfo <jrcrawfo@gmail.com>
2018-09-27 09:19:14 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ndustrialio-python',
-      version='0.2',
+      version='0.3',
       description='API bindings and worker tools for </ndustrial.io>',
       url='http://github.com/ndustrialio/ndustrialio-python',
       author='John Hunt',
@@ -14,8 +14,8 @@ setup(name='ndustrialio-python',
         'requests',
         'scipy',
         'numpy',
-	'auth0-python',
-	'pyjwt'
+        'auth0-python',
+        'pyjwt'
       ],
       dependency_links=[
       ],
